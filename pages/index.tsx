31a96b733de71d11a7c184f37380c74579679db0
gaisuke <danimunif@cs.unej.ac.id>
2022-05-16 11:09:51 +0700

    testing deployment status

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,9 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
+        <p className={styles.description}>
+          testing deployment status
+        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
